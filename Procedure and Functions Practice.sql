16231d4482113968e926c05077e1a9312c30488c
Darshan <darshanvaghela097@gmail.com>
2026-07-25 20:19:18 +0530

    Practice

--- Procedure and Functions Practice.sql
+++ Procedure and Functions Practice.sql
@@ -31,7 +31,18 @@ CALL FindBigNum(10.5,10.7);
 -- Now Same Procedure as above but what if both numbers are same --
 DELIMITER $$
 CREATE PROCEDURE FindBigNum_2 (IN Input1 Decimal (10,2),IN Input2 Decimal (10,2))
--- STOP --
+BEGIN 
+IF Input1 > Input2 THEN 
+SELECT Input1 AS "Bigger Number";
+ELSEIF Input2 > Input1 THEN
+SELECT Input2 AS "Bigger Number";
+ELSE
+SELECT 'Big Number' AS 'Equal';
+END IF;
+END$$
+DELIMITER ;
+
+CALL FindBigNum_2(10.0,10.0);
 
 -- Find Area of Circle --
 DELIMITER $$
@@ -50,5 +61,84 @@ CALL CircleArea(7);
 DELIMITER $$
 CREATE PROCEDURE SwapNum (INOUT A INT, INOUT B INT)
 BEGIN 
+DECLARE Number1 INT;
+SET Number1 = A;
+SET A=B;
+SET B=Number1;
+END$$
+DELIMITER ;
 
+SET @A = 5;
+SET @B =10;
+CALL SWAPNUM (@A,@B);
+SELECT @A AS 'A', @B AS 'B';
+
+-- Procedure to Celsius -- 
+DELIMITER $$
+CREATE PROCEDURE CelsToFahr (INOUT A FLOAT)
+BEGIN
+DECLARE Fahrenheit FLOAT;
+SET Fahrenheit = (A * 1.8) + 32;
+SET A = Fahrenheit;
+END$$
+DELIMITER ;
+
+SET @A =32;
+CALL CelstoFahr (@A);
+SELECT @A AS Fahrenheit;
+
+DELIMITER $$
+CREATE PROCEDURE PRINT (IN A FLOAT)
+BEGIN
+SELECT A AS OUTPUT;
+END$$
+DELIMITER ;
+
+CALL PRINT (@A);
+
+-- Find if number is even or odd --
+DELIMITER $$
+CREATE PROCEDURE EVENORODD (IN A INT)
+BEGIN
+IF A%2 = 0 THEN                -- % is for remainder and / is for quoteint --
+SELECT "EVEN" AS RESULT;
+ELSE 
+SELECT "ODD" AS RESULT;
+END IF;
+END$$
+DELIMITER ;
+
+CALL EVENORODD (33);
+
+DELIMITER $$
+CREATE PROCEDURE PosNegZero (IN A INT)
+BEGIN
+IF A > 0 THEN               
+SELECT "POSITIVE" AS RESULT;
+ELSEIF
+A < 0 THEN               
+SELECT "NEGATIVE" AS RESULT;
+ELSE
+SELECT "ZERO" AS RESULT;
+END IF;
+END$$
+DELIMITER ;
+
+CALL PosNegZero (24);
+CALL PosNegZero (-24);
+CALL PosNegZero (0);
+
+-- Leap Year Checker --
+DELIMITER $$
+CREATE PROCEDURE LeapYear (IN A INT)
+BEGIN
+IF (A%4=0 AND A%100 !=0) OR A%400 =0 THEN
+SELECT "LEAP YEAR" AS RESULT;
+ELSE 
+SELECT "NOT LEAP YEAR" AS RESULT;
+END IF;
+END$$
+DELIMITER ;
+
+CALL LeapYear (2025);
 
